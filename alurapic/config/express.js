1e4df6bbf106abbaefa0ff2b0b0ee3c3b8f4fb67
Fábio <fabio.henrique@touchtec.com.br>
2018-12-27 23:18:14 -0200

    Request the photos api

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -2,4 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./public'));
+app.get('/v1/fotos', function(req, res) {
+    var fotos = [
+        {_id: 1, titulo: 'Praia', url: 'https://images.unsplash.com/photo-1543988675-e050c7db2a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2082&q=80'},
+        {_id: 2, titulo: 'Oceano', url: 'https://images.unsplash.com/photo-1543839482-6a65ff225d59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80'}
+    ];
+
+    res.json(fotos);
+});
+
 module.exports = app;
