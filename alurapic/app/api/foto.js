21898fda57d3d920867c728dd47a741ea09f79bb
Fábio <fabio.henrique@touchtec.com.br>
2018-12-31 18:32:06 -0200

    Add findById API

--- alurapic/app/api/foto.js
+++ alurapic/app/api/foto.js
@@ -1,12 +1,21 @@
 var api = {};
+var fotos = [
+    {_id: 1, titulo: 'Praia', url: 'https://images.unsplash.com/photo-1543988675-e050c7db2a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2082&q=80'},
+    {_id: 2, titulo: 'Oceano', url: 'https://images.unsplash.com/photo-1543839482-6a65ff225d59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80'}
+];
+
 
 api.lista = function(req, res) {
-    var fotos = [
-        {_id: 1, titulo: 'Praia', url: 'https://images.unsplash.com/photo-1543988675-e050c7db2a21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2082&q=80'},
-        {_id: 2, titulo: 'Oceano', url: 'https://images.unsplash.com/photo-1543839482-6a65ff225d59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80'}
-    ];
-
     res.json(fotos);    
+};
+
+
+api.buscaPorId = function(req, res) {
+    var foto = fotos.find(function(data) {
+        return data._id == req.params.id;
+    });
+
+    res.json(foto);
 };
 
 module.exports = api;
