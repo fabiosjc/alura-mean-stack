61c99de845b803f7690c776511b567f23437cf7a
Fábio <fabio.henrique@touchtec.com.br>
2019-01-02 20:44:32 -0200

    Update single record

--- alurapic/app/routes/foto.js
+++ alurapic/app/routes/foto.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
     
     app.route('/v1/fotos/:id')
         .get(api.buscaPorId)
-        .delete(api.removePorId);
+        .delete(api.removePorId)
+        .put(api.atualiza);
 
 };
