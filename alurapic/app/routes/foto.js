bc3b24db8c18b8a400d851354b8d9948913ed7df
Fábio <fabio.henrique@touchtec.com.br>
2019-01-02 12:43:39 -0200

    Add 'removePorId' API

--- alurapic/app/routes/foto.js
+++ alurapic/app/routes/foto.js
@@ -1,5 +1,9 @@
 module.exports = function(app) {
     var api = app.api.foto;
+
     app.get('/v1/fotos', api.lista);
+    
     app.get('/v1/fotos/:id', api.buscaPorId);
+
+    app.delete('/v1/fotos/:id', api.removePorId);
 };
