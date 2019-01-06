78bd6b4ab3b9f9c3873921a4cd885e185f41edae
Fábio Henrique <fabiosjc@gmail.com>
2019-01-05 22:59:20 -0200

    CRUD completed

--- alurapic/app/api/foto.js
+++ alurapic/app/api/foto.js
@@ -16,19 +16,49 @@ api.lista = function(req, res) {
 };
 
 api.buscaPorId = function(req, res) {
-
+    model
+        .findById(req.params.id)
+        .then(function(foto) {
+            if (!foto) {
+                throw new Error('Foto não encontrada');
+            }
+            res.json(foto);
+        }, function (err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 api.removePorId = function(req, res) {
-
+    model
+        .findByIdAndRemove(req.params.id)
+        .then(function name(params) {
+            res.sendStatus(204);
+        }, function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 api.adiciona = function(req, res) {
-
+    model.create(req.body)
+        .then(function(foto) {
+            res.json(foto);
+        }, function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 api.atualiza = function(req, res) {
-
+    model
+        .findByIdAndUpdate(req.params.id, req.body)
+        .then(function(foto) {
+            res.json(foto);
+        }, function(err) {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 module.exports = api;
--- alurapic/app/models/foto.js
+++ alurapic/app/models/foto.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema({
     titulo: {type: String, required: true},
     url: {type: String, required: true},
+    descricao: {type: String, required: false},
     grupo: {type: Number, required: true},
 });
 
